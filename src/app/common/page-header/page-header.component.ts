c0047de968d101eda78bdffdb92a0c167310f40e
belaevslava <belaevslava@gmail.com>
2018-08-28 14:35:54 +0300

    fix header

--- src/app/common/page-header/page-header.component.ts
+++ src/app/common/page-header/page-header.component.ts
@@ -3,7 +3,10 @@ import { Component, OnInit, Input } from '@angular/core';
 @Component({
   selector: 'app-page-header',
   templateUrl: './page-header.component.html',
-  styleUrls: ['./page-header.component.scss']
+  styleUrls: [
+      './page-header.component.scss',
+      './page-header__hamburger.scss',
+  ]
 })
 export class PageHeaderComponent implements OnInit {
   @Input() theme: 'string';
